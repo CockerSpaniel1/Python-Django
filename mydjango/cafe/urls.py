a8658259fbca47df0df82833935ddabff45eed04
CockerSpaniel1 <84270403+CockerSpaniel1@users.noreply.github.com>
2024-03-16 13:04:27 +0800

    3/16 3rd(1~3) class

---
+++ mydjango/cafe/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("tea/",views.tea)
+]
+urlpatterns = [
+    path("tea1/",views.tea1)
+]
+urlpatterns = [
+    path("coffee2/",views.coffee2)
+]
---
+++ mydjango/static/js/yourjs.js
@@ -0,0 +1,9 @@
+function onsale(){
+    var name=document.getElementById("name");
+    var price=document.getElementById("price");
+    var qty=document.getElementById("qty");
+    document.getElementById("demo").innerHTML =  "咖啡名稱" + name.value + "<br>"
+                                                +"咖啡價格" + price.value + "<br>"
+                                                +"訂購數量" + qty.value + "<br>"
+                                                +"訂購總額" + (price.value * qty.value) + "<br>"
+}
